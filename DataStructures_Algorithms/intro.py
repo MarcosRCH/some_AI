e1af3f5cdb4fbf3cf5bcd40d162c3a9a1dfc5715
MarcosRCH <rodriguezchamorromarcos@gmail.com>
2022-11-22 23:49:01 +0100

    Update intro.py

--- DataStructures_Algorithms/intro.py
+++ DataStructures_Algorithms/intro.py
@@ -19,25 +19,77 @@ class Classy(object):
     def addItem(self, item):
         self.items.append(item)
 
-    def getClassiness(self, any):
+    def getClassiness(self, items):
+        _dicc_items = {"tophat": 2, "bowtie": 4, "monocle": 5}
+        for item in items:
+            if item in _dicc_items[item]:
+
+
         return sum(self.items())
 
 # Test cases
 me = Classy()
 
 # Should be 0
-print me.getClassiness()
+print(me.getClassiness())
 
 me.addItem("tophat")
 # Should be 2
-print me.getClassiness()
+print(me.getClassiness())
 
 me.addItem("bowtie")
 me.addItem("jacket")
 me.addItem("monocle")
 # Should be 11
-print me.getClassiness()
+print(me.getClassiness())
 
 me.addItem("bowtie")
 # Should be 15
-print me.getClassiness()
+print(me.getClassiness())
+
+
+
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+    imprimir = " ".join(["I am super excited for this course!"]*5)
+    return imprimir
+
+print(show_excitement())
+
+
+
+
+"""input manatees: a list of "manatees", where one manatee is represented by a dictionary
+a single manatee has properties like "name", "age", et cetera
+n = the number of elements in "manatees"
+m = the number of properties per "manatee" (i.e. the number of keys in a manatee dictionary)"""
+
+def example1(manatees):
+    for manatee in manatees:
+        print manatee['name']
+
+def example2(manatees):
+    print manatees[0]['name']
+    print manatees[0]['age']
+
+def example3(manatees):
+    for manatee in manatees:
+        for manatee_property in manatee:
+            print manatee_property, ": ", manatee[manatee_property]
+
+def example4(manatees):
+    oldest_manatee = "No manatees here!"
+    for manatee1 in manatees:
+        for manatee2 in manatees:
+            if manatee1['age'] < manatee2['age']:
+                oldest_manatee = manatee2['name']
+            else:
+                oldest_manatee = manatee1['name']
+    print oldest_manatee
